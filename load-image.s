495fa9c4b4c4776ce5a22b0911e765b7f1a12b67
Andre Baptista <albs_br@yahoo.com.br>
2021-09-28 01:03:58 -0300

    v.0.3.0

--- load-image.s
+++ load-image.s
@@ -21,16 +21,33 @@ Execute:
     ld      a, 5
     call    BIOS_CHGMOD
 
+    ; Das 16 cores da paleta, a cor 0 é transparente, ou seja, não pode
+    ; ser definida uma cor para ela e qualquer objeto desenhado com ela não
+    ; será visto. Entretanto, setando o bit 5 de R#8, a função de transparente
+    ; será desativada e a cor 0 poderá ser definida por P#0.    
+    ; set color 0 to non transparent
+    ld      b, 0010 0000 b  ; data
+    ld      c, 0x08         ; register #
+    call    BIOS_WRTVDP
+    
     ; set 192 lines
     ld      b, 0000 0000 b  ; data
     ld      c, 0x09         ; register #
     call    BIOS_WRTVDP
+
     
 	; enable page 2
     ld	    a, 2
 	ld	    (Seg_P8000_SW), a
     ; load 32-byte palette data
-    ld      hl, ImageData_2.palette ;PaletteData
+    ld      hl, ImageData_2.palette ; PaletteData
+                    ; ; debug
+                    ; ld      a, (hl)
+                    ; ld      (debug_0), a
+                    ; inc     hl
+                    ; ld      a, (hl)
+                    ; ld      (debug_1), a
+                    ; ld      hl, ImageData_2.palette ; PaletteData
     call    LoadPalette
 
 	; enable page 1
@@ -54,29 +71,40 @@ Execute:
 .endlessLoop:
     jp      .endlessLoop
 
-PaletteData:
-    INCBIN "Images/palette.bin"
+;PaletteData:
+    ;INCBIN "Images/aerofighters-palette.bin"
+    ;INCBIN "Images/metalslug-palette.bin"
 
 
 End:
 
     db      "End ROM started at 0x4000"
 
-	ds PageSize - ($ - 0x4000), 255	; Fill the unused area with 0FFh
+	ds PageSize - ($ - 0x4000), 255	; Fill the unused area with 0xFF
 
 
 ; MegaROM pages at 0x8000
 ; ------- Page 1
 	org	0x8000, 0xBFFF
 ImageData:
-    INCBIN "Images/xaa"
+    ;INCBIN "Images/aerofighters-xaa"
+    INCBIN "Images/metalslug-xaa"
 .size:      equ $ - ImageData
 	ds PageSize - ($ - 0x8000), 255
 
 ; ------- Page 2
 	org	0x8000, 0xBFFF
 ImageData_2:
-    INCBIN "Images/xab"
+    ;INCBIN "Images/aerofighters-xab"
+    INCBIN "Images/metalslug-xab"
 .size:      equ $ - ImageData_2
 .palette:   equ $ - 32
 	ds PageSize - ($ - 0x8000), 255
+
+
+
+; RAM
+	org     0xc000, 0xe5ff                   ; for machines with 16kb of RAM (use it if you need 16kb RAM, will crash on 8kb machines, such as the Casio PV-7)
+
+debug_0:    rb 1
+debug_1:    rb 1
---
+++ msx-tests_debug.tcl
@@ -0,0 +1,4 @@
+
+ram_watch   add     0xc000      -type byte      -desc debug_0     -format hex
+ram_watch   add     0xc001      -type byte      -desc debug_1     -format hex
+
