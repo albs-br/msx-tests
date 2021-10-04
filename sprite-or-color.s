ffacf4680eab6ac1e0318b0a018a81ef14c96c3d
Andre Baptista <albs_br@yahoo.com.br>
2021-10-04 00:24:46 -0300

    v.0.6.5

--- sprite-or-color.s
+++ sprite-or-color.s
@@ -1,25 +1,24 @@
 FNAME "sprite-or-color.rom"      ; output file
 
 PageSize:	    equ	0x4000	        ; 16kB
-Seg_P8000_SW:	equ	0x7000	        ; Segment switch for page 0x8000-BFFFh (ASCII 16k Mapper)
 
 ; Compilation address
-    org 0x4000, 0xbeff	                    ; 0x8000 can be also used here if Rom size is 16kB or less.
+    org 0x4000, 0x7fff	                    ; 0x8000 can be also used here if Rom size is 16kB or less.
 
     INCLUDE "Include/RomHeader.s"
     INCLUDE "Include/MsxBios.s"
     INCLUDE "Include/CommonRoutines.s"
 
 Execute:
-    call    EnableRomPage2
 
-	; enable page 1
-    ld	    a, 1
-	ld	    (Seg_P8000_SW), a
 
     ; change to screen 11
-    ld      a, 11
+    ; it's needed to set screen 8 and change the YJK and YAE bits of R#25 manually
+    ld      a, 8
     call    BIOS_CHGMOD
+    ld      b, 0001 1000 b      ; data
+    ld      c, 25               ; register #
+    call    BIOS_WRTVDP
 
     call    BIOS_DISSCR
 
@@ -38,27 +37,27 @@ Execute:
     ; ld      c, 2            ; register #
     ; call    BIOS_WRTVDP
 
-; ---- set SPRATR to 0x1fa00 (SPRCOL is automatically set 512 bytes before SPRATR, so 0x1f800)
-    ; bits:    16 14        7
-    ;           |  |        |
-    ; 0x1fa00 = 1 1111 1010 1000 0000
-    ; low bits (aaaaaaaa: bits 14 to 7)
-    ld      b, 1111 0101 b  ; data
-    ld      c, 5            ; register #
-    call    BIOS_WRTVDP
-    ; high bits (000000aa: bits 16 to 15)
-    ld      b, 0000 0011 b  ; data
-    ld      c, 11           ; register #
-    call    BIOS_WRTVDP
+; ; ---- set SPRATR to 0x1fa00 (SPRCOL is automatically set 512 bytes before SPRATR, so 0x1f800)
+;     ; bits:    16 14        7
+;     ;           |  |        |
+;     ; 0x1fa00 = 1 1111 1010 1000 0000
+;     ; low bits (aaaaaaaa: bits 14 to 7)
+;     ld      b, 1111 0101 b  ; data
+;     ld      c, 5            ; register #
+;     call    BIOS_WRTVDP
+;     ; high bits (000000aa: bits 16 to 15)
+;     ld      b, 0000 0011 b  ; data
+;     ld      c, 11           ; register #
+;     call    BIOS_WRTVDP
 
-; ---- set SPRPAT to 0x1f000
-    ; bits:    16     11
-    ;           |      |
-    ; 0x1fa00 = 1 1111 0000 0000 0000
-    ; high bits (00aaaaaa: bits 16 to 11)
-    ld      b, 0011 1110 b  ; data
-    ld      c, 6            ; register #
-    call    BIOS_WRTVDP
+; ; ---- set SPRPAT to 0x1f000
+;     ; bits:    16     11
+;     ;           |      |
+;     ; 0x1fa00 = 1 1111 0000 0000 0000
+;     ; high bits (00aaaaaa: bits 16 to 11)
+;     ld      b, 0011 1110 b  ; data
+;     ld      c, 6            ; register #
+;     call    BIOS_WRTVDP
 
 NAMTBL:     equ 0x0000
 SPRPAT:     equ 0xf000 ; actually 0x1f000, but 17 bits address are not accepted
@@ -127,87 +126,22 @@ SPRATR:     equ 0xfa00
     ld      c, 14            ; register #
     call    BIOS_WRTVDP
 
-
-ADDR_LAST_LINE_OF_PAGE: equ 0x8000 + (63 * 256)
-
-    ; initialize variables for scrolling on last line of the next page
-    ld      a, 13
-    ld      (CurrentMegaROMPage), a
-    ld      hl, ADDR_LAST_LINE_OF_PAGE
-    ld      (CurrentAddrLineScroll), hl
-    ld      hl, 255 * 256
-    ld      (CurrentVRAMAddrLineScroll), hl
-
-
-    xor     a
-    ld      (VerticalScroll), a
-
-.loop:
-    ld      a, (BIOS_JIFFY)
-    ld      b, a
-.waitVBlank:
-    ld      a, (BIOS_JIFFY)
-    cp      b
-    jp      z, .waitVBlank
-
-    ;call    Wait
-.endlessLoop:
-    jp  .endlessLoop
-
-    ; load next line from bitmap on the last line of virtual screen (256 lines)
-    ; that will be the next to be shown on top of screen
-    ld	    a, (CurrentMegaROMPage)
-	ld	    (Seg_P8000_SW), a
-    ld      hl, (CurrentAddrLineScroll)             ; RAM address (source)
-    ld		de, (CurrentVRAMAddrLineScroll)         ; VRAM address (destiny)
-    ld		bc, 256					                ; Block length
+    ; load test image
+    ld		hl, ImageTest 				            ; RAM address (source)
+    ld		de, NAMTBL                              ; VRAM address (destiny)
+    ld		bc, ImageTest.size                      ; Block length
     call 	BIOS_LDIRVM        						; Block transfer to VRAM from memory
-	; ld	    hl, (CurrentVRAMAddrLineScroll)		; VRAM start address
-    ; ld      bc, 256                             ; number of bytes
-    ; ld      a, 00011100 b                       ; value
-    ; call    BIOS_FILVRM                         ; Fill VRAM
 
-    ; update vars
-    ld      de, (CurrentVRAMAddrLineScroll)
-    dec     d                                       ; de = de - 256
-    ld      hl, (CurrentAddrLineScroll)
-    dec     h                                       ; hl = hl - 256
-    ld      a, h
-    cp      0x80 - 1
-    jp      z, .decPage
-    jp      .dontDecPage
-.decPage:
-    ld      a, (CurrentMegaROMPage)
-    dec     a
-    jp      z, .stopScroll
-    ld      (CurrentMegaROMPage), a
-    ld      hl, ADDR_LAST_LINE_OF_PAGE
-.dontDecPage:
-    ld      (CurrentAddrLineScroll), hl
-    ld      (CurrentVRAMAddrLineScroll), de
-
-
-    ; vertical scroll
-    ld      hl, VerticalScroll
-    dec     (hl)
-    ld      b, (hl)         ; data
-    ld      c, 23           ; register #
-    call    BIOS_WRTVDP
-
-
-
-    jp      .loop
-
-.stopScroll:
-    jp      .stopScroll
+.endlessLoop:
+    jp      .endlessLoop
 
 
 End:
 
 SpritePattern_1:
     DB 11111111b
-    DB 11111111b
-    DB 11111111b
+    DB 11000011b
+    DB 11000011b
     DB 11111111b
     DB 11111111b
     DB 11111111b
@@ -239,140 +173,29 @@ SpritePattern_1:
     DB 11111111b
 
 SpriteColors_1:
-    db 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a, 0x0a
-
-    db      "End ROM started at 0x4000"
-
-	ds PageSize - ($ - 0x4000), 255	; Fill the unused area with 0xFF
+    db 0x02, 0x0a, 0x03, 0x03, 0x08, 0x08, 0x03, 0x0a, 0x04, 0x07, 0x0a, 0x0a, 0x0a, 0x0a, 0x0f, 0x0f
 
 SpriteAttributes_1:
     db 88, 120, 0, 0
 .size:  equ $ - SpriteAttributes_1
 
-; MegaROM pages at 0x8000
-; ------- Page 1
-	org	0x8000, 0xBFFF
-ImageData_1:
-    INCBIN "Images/aerofighters_0.sra.new"
-.size:      equ $ - ImageData_1
-	ds PageSize - ($ - 0x8000), 255
 
-; ------- Page 2
-	org	0x8000, 0xBFFF
-ImageData_2:
-    INCBIN "Images/aerofighters_1.sra.new"
-.size:      equ $ - ImageData_2
-	ds PageSize - ($ - 0x8000), 255
+ImageTest:
+    db  0xff, 0xff, 0xff, 0xff
+    db  0xff, 0xff, 0xff, 0xff
+    db  0xff, 0xff, 0xff, 0xff
+    db  0xff, 0xff, 0xff, 0xff
+    db  0xff, 0xff, 0xff, 0xff
+    db  0xff, 0xff, 0xff, 0xff
+    db  0xff, 0xff, 0xff, 0xff
+    db  0xff, 0xff, 0xff, 0xff
+.size:  equ $ - ImageTest
 
-; ------- Page 3
-	org	0x8000, 0xBFFF
-ImageData_3:
-    INCBIN "Images/aerofighters_2.sra.new"
-.size:      equ $ - ImageData_3
-	ds PageSize - ($ - 0x8000), 255
+    db      "End ROM started at 0x4000"
 
-; ------- Page 4
-	org	0x8000, 0xBFFF
-ImageData_4:
-    INCBIN "Images/aerofighters_3.sra.new"
-.size:      equ $ - ImageData_4
-	ds PageSize - ($ - 0x8000), 255
-
-; ------- Page 5
-	org	0x8000, 0xBFFF
-ImageData_5:
-    INCBIN "Images/aerofighters_4.sra.new"
-.size:      equ $ - ImageData_5
-	ds PageSize - ($ - 0x8000), 255
-
-; ------- Page 6
-	org	0x8000, 0xBFFF
-ImageData_6:
-    INCBIN "Images/aerofighters_5.sra.new"
-.size:      equ $ - ImageData_6
-	ds PageSize - ($ - 0x8000), 255
-
-; ------- Page 7
-	org	0x8000, 0xBFFF
-ImageData_7:
-    INCBIN "Images/aerofighters_6.sra.new"
-.size:      equ $ - ImageData_7
-	ds PageSize - ($ - 0x8000), 255
-
-; ------- Page 8
-	org	0x8000, 0xBFFF
-ImageData_8:
-    INCBIN "Images/aerofighters_7.sra.new"
-.size:      equ $ - ImageData_8
-	ds PageSize - ($ - 0x8000), 255
-
-; ------- Page 9
-	org	0x8000, 0xBFFF
-ImageData_9:
-    INCBIN "Images/aerofighters_8.sra.new"
-.size:      equ $ - ImageData_9
-	ds PageSize - ($ - 0x8000), 255
-
-; ------- Page 10
-	org	0x8000, 0xBFFF
-ImageData_10:
-    INCBIN "Images/aerofighters_9.sra.new"
-.size:      equ $ - ImageData_10
-	ds PageSize - ($ - 0x8000), 255
-
-; ------- Page 11
-	org	0x8000, 0xBFFF
-ImageData_11:
-    INCBIN "Images/aerofighters_10.sra.new"
-.size:      equ $ - ImageData_11
-	ds PageSize - ($ - 0x8000), 255
-
-; ------- Page 12
-	org	0x8000, 0xBFFF
-ImageData_12:
-    INCBIN "Images/aerofighters_11.sra.new"
-.size:      equ $ - ImageData_12
-	ds PageSize - ($ - 0x8000), 255
-
-; ------- Page 13
-	org	0x8000, 0xBFFF
-ImageData_13:
-    INCBIN "Images/aerofighters_12.sra.new"
-.size:      equ $ - ImageData_13
-	ds PageSize - ($ - 0x8000), 255
-
-; ------- Page 14
-	org	0x8000, 0xBFFF
-ImageData_14:
-    INCBIN "Images/aerofighters_13.sra.new"
-.size:      equ $ - ImageData_14
-	ds PageSize - ($ - 0x8000), 255
-
-; ------- Page 15
-	org	0x8000, 0xBFFF
-ImageData_15:
-    INCBIN "Images/aerofighters_14.sra.new"
-.size:      equ $ - ImageData_15
-	ds PageSize - ($ - 0x8000), 255
-
-; ------- Page 16
-	org	0x8000, 0xBFFF
-ImageData_16:
-    INCBIN "Images/aerofighters_15.sra.new"
-.size:      equ $ - ImageData_16
-	ds PageSize - ($ - 0x8000), 255
-
+	ds PageSize - ($ - 0x4000), 255	; Fill the unused area with 0xFF
 
 
 ; RAM
 	org     0xc000, 0xe5ff                   ; for machines with 16kb of RAM (use it if you need 16kb RAM, will crash on 8kb machines, such as the Casio PV-7)
 
-
-VerticalScroll:             rb 1
-CurrentMegaROMPage:         rb 1
-CurrentAddrLineScroll:      rw 1
-CurrentVRAMAddrLineScroll:  rw 1
-
-
-debug_0:    rb 1
-debug_1:    rb 1
