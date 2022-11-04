e4756aaf1fd826f2a3f936510d1f877412a31969
ANDRE LUIS BAPTISTA DA SILVA <andrebaptista@petrobras.com.br>
2022-11-04 15:42:34 -0300

    v.0.29.2

--- v9990.s
+++ v9990.s
@@ -63,6 +63,50 @@ V9:
     ret
 
 
+; Write VRAM from RAM
+.LDIRVM:
+    push    bc
+        push    af
+            ; set P#4 to 0000 0000 b
+            ld      a, 0000 0000 B
+            out     (V9.PORT_4), a
+
+            ld      c, V9.PORT_3
+
+            ; set P#3 to VRAM lower addr (bits 0-7)
+            out     (c), e
+
+            ; set P#3 to VRAM center addr (bits 8-15)
+            out     (c), d
+        pop     af
+
+        ; set P#3 to VRAM upper addr (bits 16-18) --> warning: higher bit here is AII (explained above)
+        and     0111 1111 b     ; force AII bit to 0
+        out     (c), a
+    pop     de
+
+
+    ;out     (c), a
+
+    ld      b, 32
+    
+    ld      c, V9.PORT_0
+.LDIRVM_loop:
+    ; set P#0 to value to be written
+    outi
+    
+    djnz    .LDIRVM_loop
+
+    ; dec     e
+    ; jp      nz, .LDIRVM_loop
+    ; dec     d
+    ; jp      nz, .LDIRVM_loop
+
+    ret
+
+
+
+
 Execute:
 
     ; ------- set P1 mode
@@ -77,7 +121,7 @@ Execute:
     ; set CLRM = 1 (bits 1-0) of R#6
     ld      a, 6            ; register number
     ld      b, 0000 0101 b  ; value
-    call    SetRegister
+    call    V9.SetRegister
 
     ; bit 7 of R#7 is fixed at 0
     ; set C25M = 0 (bit 6) of R#7
@@ -91,14 +135,108 @@ Execute:
     ld      b, 0000 0000 b  ; value
     call    V9.SetRegister
 
+
+    ; set control register (R#8)
+    ; set DISP = 1 (bit 7) of R#8
+    ld      a, 8            ; register number
+    ld      b, 1000 0010 b  ; value
+    call    V9.SetRegister
+
+
+    ; set priority control register (R#27)
+    ld      a, 27           ; register number
+    ld      b, 0000 0000 b  ; value
+    call    V9.SetRegister
+
+
+
+    ; ------- set scroll control registers (R#17 to R#24)
+    ld      a, 17           ; register number
+    ld      b, 0000 0000 b  ; value
+    call    V9.SetRegister
+
+    ld      a, 18           ; register number
+    ld      b, 0000 0000 b  ; value
+    call    V9.SetRegister
+
+    ld      a, 19           ; register number
+    ld      b, 0000 0000 b  ; value
+    call    V9.SetRegister
+
+    ld      a, 20           ; register number
+    ld      b, 0000 0000 b  ; value
+    call    V9.SetRegister
+
+    ld      a, 21           ; register number
+    ld      b, 0000 0000 b  ; value
+    call    V9.SetRegister
+
+    ld      a, 22           ; register number
+    ld      b, 0000 0000 b  ; value
+    call    V9.SetRegister
+
+    ld      a, 23           ; register number
+    ld      b, 0000 0000 b  ; value
+    call    V9.SetRegister
+
+    ld      a, 24           ; register number
+    ld      b, 0000 0000 b  ; value
+    call    V9.SetRegister
+
+
+
     ; ------- set tile pattern
-    ld		hl, ImageData        				    ; RAM address (source)
-    ld		a, 0					                ; VRAM address bits 18-16 (destiny)
-    ld		de, 0					                ; VRAM address bits 15-0 (destiny)
-    ld		bc, ImageData.size					    ; Block length
-    call 	V9.LDIRVM        						; Block transfer to VRAM from memory
+    ld		hl, Tile_0        				        ; RAM address (source)
+    ld		a, V9.P1_PATTBL_LAYER_A >> 16	        ; VRAM address bits 18-16 (destiny)
+    ld		de, V9.P1_PATTBL_LAYER_A AND 0xffff     ; VRAM address bits 15-0 (destiny)
+    ld		bc, Tile_0.size					        ; Block length
+    call 	V9.LDIRVM        					    ; Block transfer to VRAM from memory
+
 
     ; ------- set names table
+    ld		hl, NamesTable_test				        ; RAM address (source)
+    ld		a, V9.P1_NAMTBL_LAYER_A >> 16	        ; VRAM address bits 18-16 (destiny)
+    ld		de, V9.P1_NAMTBL_LAYER_A AND 0xffff     ; VRAM address bits 15-0 (destiny)
+    ld		bc, NamesTable_test.size		        ; Block length
+    call 	V9.LDIRVM        					    ; Block transfer to VRAM from memory
+
+
+    ; ------- set palette control register (R#13)
+    
+    ; Background colors are specified by Pattern data plus a palette offset in R#13. P1 layer "A" and 
+    ; P2 pattern pixels 0,1,4,5 use offset specified in R#13 PLTO3-2. P1 layer "B" and P2 pattern 
+    ; pixels 2,3,6,7 use offset specified in R#13 PLTO5-4.
+
+    ; set PLTM to 00 on R#13 (bits 7-6)
+    ; set YAE to 0 on R#13 (bit 5)
+    ; set PLTAIH to 0 on R#13 (bit 4)
+    ; set PLTO2-5 to 0 on R#13 (bits 0-3)
+    ld      a, 13           ; register number
+    ld      b, 0000 00 00 b  ; value
+    call    V9.SetRegister
+
+
+    ; --------- set palette
+
+    ; set 0000 1110 b to P#4
+    ld      a, 0000 1110 b
+    out     (V9.PORT_4), a
+
+    ; set palette number (6 higher bits) to P#3; 2 lower bits to 00
+    ld      a, 0000 0000 b
+    out     (V9.PORT_3), a
+
+    ; set RED value (5 bits, 0-31 value) to P#1
+    ; set GREEN value (5 bits, 0-31 value) to P#1
+    ; set BLUE value (5 bits, 0-31 value) to P#1
+    ld      hl, Palette_test
+    ld      c, V9.PORT_1
+    ld      b, 16 * 3   ; number of colors * 3
+.SetPalette_loop:
+    outi    ; red
+    outi    ; green
+    outi    ; blue
+    djnz    .SetPalette_loop
 
     jp      $   ; eternal loop
 
@@ -127,56 +265,68 @@ Execute:
 ; if AII is 0, write next bytes to sequentially P#0
 
 
-.LDIRVM:
-    push    bc
-        push    af
-            ld      c, V9.PORT_4
-            
-            ; set P#4 to 0000 0000 b
-            ld      a, 0000 0000 B
-            out     (c), a
-
-            ld      c, V9.PORT_3
-
-            ; set P#3 to VRAM lower addr (bits 0-7)
-            out     (c), e
-
-            ; set P#3 to VRAM center addr (bits 8-15)
-            out     (c), d
-        pop     af
-
-        ; set P#3 to VRAM upper addr (bits 16-18) --> warning: higher bit here is AII (explained above)
-        and     0111 1111 b     ; force AII bit to 0
-        out     (c), a
-    pop     de
-
-
-    ;out     (c), a
-
-    ld      c, V9.PORT_0
-.LDIRVM_loop:
-    ; set P#0 to value to be written
-    outi
-    
-    dec     e
-    jp      nz, .LDIRVM_loop
-    dec     d
-    jp      nz, .LDIRVM_loop
-
-    ret
-
-
 
 ; -----------------------------------------------------------------
 
+; VRAM data
 
 ; 8x8 x 4bpp tile
 Tile_0:
     db  0xff, 0xff, 0xff, 0xff
     db  0xf0, 0x00, 0x00, 0x0f
+    db  0xf6, 0x12, 0x34, 0x5f
+    db  0xf7, 0x89, 0xab, 0xcf
+    db  0xfd, 0xef, 0xff, 0xff
     db  0xff, 0xff, 0xff, 0xff
     db  0xff, 0xff, 0xff, 0xff
     db  0xff, 0xff, 0xff, 0xff
-    db  0xff, 0xff, 0xff, 0xff
-    db  0xff, 0xff, 0xff, 0xff
-    db  0xff, 0xff, 0xff, 0xff
+.size:      equ $ - Tile_0
+
+NamesTable_test:
+    dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+    ; dw  0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1
+.size:      equ $ - NamesTable_test
+
+Palette_test:
+    ;   R   G   B   (5 bits, 0-31 value)
+    db  15,  0,  0
+    db  15, 31, 31
+    db  15,  0, 31
+    db  15, 15, 15
+
+    db   0,  0,  0
+    db  31,  0,  0
+    db   0, 31,  0
+    db   0,  0, 31
+
+    db  31, 15, 15
+    db  15, 31, 15
+    db  15, 15, 31
+    db  15,  0,  0
+
+    db   0, 15,  0
+    db   0,  0, 15
+    db   0,  7,  0
+    db   0,  0,  7
