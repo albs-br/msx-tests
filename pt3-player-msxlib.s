3af2b89c6cca9c8e9ccdfe21761ee5f269870c52
Andre Baptista <albs_br@yahoo.com.br>
2023-09-30 20:37:43 -0300

    v.0.47.0

--- pt3-player-msxlib.s
+++ pt3-player-msxlib.s
@@ -10,24 +10,30 @@ PageSize:	    equ	0x4000	        ; 16kB
     INCLUDE "Include/MsxConstants.s"
     INCLUDE "Include/CommonRoutines.s"
 
+
+; INCLUDES from msxlib:
+	include	"include/msxlib/asm.asm"
+
+
+
 ; -----------------------------------------------------------------------------
 
 SONG_TABLE:
 	dw	.PT3_Music_sample
-	dw	.empty
+	; dw	.empty
 .PT3_Music_sample:
-    INCBIN "Sound/StayorGo.pt3"
-.empty:
-	incbin	"games/minigames/run23/music/empty.pt3.hl.zx0"
+    INCBIN "Sound/StayorGo.pt3", 100
+; .empty:
+; 	incbin	"games/minigames/run23/music/empty.pt3.hl.zx0"
 
-; SOUND_BANK:
-; 	incbin	"games/minigames/run23/music/run23.afb"
+SOUND_BANK:
+	; incbin	"games/minigames/run23/music/run23.afb"
 
-; 	CFG_SOUND_JOIN:			equ 1 -1
-; 	CFG_SOUND_PUSH:			equ 2 -1
-; 	CFG_SOUND_TIMEOUT:		equ 3 -1
-; 	CFG_SOUND_WEIGHT_MOVE:		equ 5 -1
-; 	CFG_SOUND_WEIGHT_FALL:		equ 4 -1
+	; CFG_SOUND_JOIN:			equ 1 -1
+	; CFG_SOUND_PUSH:			equ 2 -1
+	; CFG_SOUND_TIMEOUT:		equ 3 -1
+	; CFG_SOUND_WEIGHT_MOVE:		equ 5 -1
+	; CFG_SOUND_WEIGHT_FALL:		equ 4 -1
 
 
 ; -----------------------------------------------------------------------------
@@ -49,37 +55,35 @@ SONG_TABLE:
 
 
 ; ---- init new game
-; No music
-	ld	a, 2
-	call	REPLAYER.PLAY
+; ; No music
+; 	ld	a, 2
+; 	call	REPLAYER.PLAY
+
+
+; ; Starts the music
+; 	xor	a
+; 	call	REPLAYER.PLAY
 
 
 ; Starts the music
-	xor	a
+	; ld	a, $80 + 1
+	ld	a, $80 + 0
 	call	REPLAYER.PLAY
 
 
-; Starts the music
-	ld	a, $80 + 1
-	call	REPLAYER.PLAY
-
-
-; Stops the music
-	jp	REPLAYER.STOP
+; ; Stops the music
+; 	jp	REPLAYER.STOP
 
 
 
 
 Execute:
 
-    ; pt3 player need to be on RAM, as it uses self-modifying code
-    ld	    hl, pt3_player
-    ld	    de, pt3_player_ram_addr
-    ld	    bc, pt3_player.size
-    ldir
+    ; ld      hl, Debug_Message
+    ; call    PrintString
 
-    ld      hl, PT3_Music_sample
-    call    INIT
+    call    BIOS_INITXT
+
 
 .loop:
 
@@ -91,10 +95,44 @@ Execute:
     cp      b
     jp      z, .waitVBlank
 
-    call    PLAY
+; Invokes the replayer
+IFEXIST REPLAYER.FRAME
+; Invokes the replayer (with frameskip in 60Hz machines)
+	ld	a, 5 ;6 ; [frames_per_tenth]
+	cp	5
+	jr	z, .NO_FRAMESKIP ; No frameskip (50Hz machine)
+; Checks frameskip (60Hz machine)
+	; ld	a, 6 ; (unnecessary)
+	ld	hl, replayer.frameskip
+	inc	[hl]
+	sub	[hl]
+	jr	nz, .NO_FRAMESKIP ; No framewksip
+; Resets frameskip counter
+	; xor	a ; (unnecessary)
+	ld	[hl], a
+	jr	.FRAMESKIP
+
+.NO_FRAMESKIP:
+; Executes a frame of the replayer
+	call	REPLAYER.FRAME
+.FRAMESKIP:
+ENDIF ; REPLAYER.FRAME
 
     ; call    BIOS_BEEP
+
     jp      .loop
+
+
+PrintString:
+    ld      a, (hl)
+    cp      0
+    ret     z
+    call    BIOS_CHPUT
+    inc     hl
+    jr      PrintString
+
+Debug_Message:
+    db      "Test message", 0
 
     db      "End ROM started at 0x4000"
 
@@ -105,5 +143,15 @@ Execute:
 ; RAM
 	org     0xc000, 0xe5ff
 
+; ; -----------------------------------------------------------------------------
+; ; Unpacker routine buffer
+; unpack_buffer:
+; 	rb	CFG_RAM_RESERVE_BUFFER
 
-pt3_player_ram_addr:     rb pt3_player.size
+	include	"include/PT3-RAM.tniasm.asm"
+
+	include	"include/ayFX-RAM.tniasm.asm"
+
+; 60Hz replayer synchronization
+replayer.frameskip:
+	rb	1
