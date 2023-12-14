3a182757b174e7c5486e0815be56d603b2b3e944
Andre Baptista <albs_br@yahoo.com.br>
2023-12-14 17:04:58 -0300

    v.0.49.7

--- sprite-or-color.s
+++ sprite-or-color.s
@@ -154,10 +154,10 @@ SpritePattern_0_and_1:
     ; INCLUDE "Images/font_char_0_patterns.s"
     ; INCBIN "Images/enemy_plane_turning_0.pat"
     ; INCLUDE "Images/item_bomb_0_patterns.s"
-    INCBIN "Images/hovercraft_frame_0_top_left.pat"
-    INCBIN "Images/hovercraft_frame_0_top_right.pat"
-    INCBIN "Images/hovercraft_frame_0_bottom_left.pat"
-    ; INCLUDE "Images/tank_top_right-patterns.s"
+    INCBIN "Images/enemy_hovercraft_frame_1_top_left.pat"
+    INCBIN "Images/enemy_hovercraft_frame_1_top_right.pat"
+    INCBIN "Images/enemy_hovercraft_frame_1_bottom_left.pat"
+    INCLUDE "Images/enemy_hovercraft_frame_1_bottom_right-patterns.s"
 .size:  equ $ - SpritePattern_0_and_1
 
 ; SpritePattern_2_and_3:
@@ -181,10 +181,10 @@ SpriteColors_1:
     ; INCLUDE "Images/font_char_0_colors.s"
     ; INCBIN "Images/enemy_plane_turning_0.col"
     ; INCLUDE "Images/item_bomb_0_colors.s"
-    INCBIN "Images/hovercraft_frame_0_top_left.col"
-    INCBIN "Images/hovercraft_frame_0_top_right.col"
-    INCBIN "Images/hovercraft_frame_0_bottom_left.col"
-    ; INCLUDE "Images/tank_top_right-colors.s"
+    INCBIN "Images/enemy_hovercraft_frame_1_top_left.col"
+    INCBIN "Images/enemy_hovercraft_frame_1_top_right.col"
+    INCBIN "Images/enemy_hovercraft_frame_1_bottom_left.col"
+    INCLUDE "Images/enemy_hovercraft_frame_1_bottom_right-colors.s"
 .size:  equ $ - SpriteColors_1
 
 ; SpriteColors_2:
@@ -208,8 +208,8 @@ SpriteAttributes_top:
     db  90+16,    100+2,        4 * 4,      0
     db  90+16,    100+2,        5 * 4,      0
 
-    ; ; sprite 6
-    ; db  90-16, 100 + 7 +16,     6 * 4,      0
+    ; sprite 6
+    db  90+16, 100+2+16,        6 * 4,      0
 
 .size:  equ $ - SpriteAttributes_top
 
