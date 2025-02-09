afcffada98e05a481ec4ce911c45f98eb42ecd8f
Andre Baptista <albs_br@yahoo.com.br>
2025-02-09 20:53:53 -0300

    v.0.61.1

--- triple-buffer.s
+++ triple-buffer.s
@@ -30,17 +30,16 @@ Execute:
 
     call    SetColor0ToNonTransparent
 
+    call    DisableSprites
+
 
     
     ; load 32-byte palette data
     ld      hl, Palette
     call    LoadPalette
 
-	; enable page 1
-    ld	    a, 1
-	ld	    (Seg_P8000_SW), a
-    
-    ; write to VRAM bitmap area
+   
+    ; --- Write to VRAM bitmap area
 
     ; SC 5 - page 0
     ld      a, 0000 0000 b
@@ -63,16 +62,6 @@ Execute:
     call    LoadImageTo_SC5_Page
 
 
-	; ; enable page 2
-    ; ld	    a, 2
-	; ld	    (Seg_P8000_SW), a
-    ; ; write to VRAM bitmap area
-    ; ld		hl, ImageData_2      				    ; RAM address (source)
-    ; ld		de, 16384			                    ; VRAM address (destiny)
-    ; ld		bc, ImageData_2.size					; Block length
-    ; call 	BIOS_LDIRVM        						; Block transfer to VRAM from memory
-
-
 
     call    BIOS_ENASCR
 
@@ -83,15 +72,45 @@ Execute:
 ; Input:
 ;   AHL: 17-bit VRAM address
 LoadImageTo_SC5_Page:
+	; enable page 1
+    push    af
+        ld	    a, 1
+        ld	    (Seg_P8000_SW), a
+    pop     af
+
+    ; first 16kb (top 128 lines)
+    push    af, hl
+        call    SetVdp_Write
+        ld      hl, Bg_Top
+        ld      c, PORT_0
+        ld      d, 0 + (Bg_Top.size / 256)
+        ld      b, 0 ; 256 bytes
+    .loop_10:    
+        otir
+        dec     d
+        jp      nz, .loop_10
+    pop     hl, af
+
+	; enable page 2
+    push    af
+        ld	    a, 2
+        ld	    (Seg_P8000_SW), a
+    pop     af
+
+    ; lines below 128
+    ld      bc, 16 * 1024
+    add     hl, bc
+
     call    SetVdp_Write
-    ld      hl, ImageData
+    ld      hl, Bg_Bottom
     ld      c, PORT_0
-    ld      d, 0 + (ImageData.size / 256)
+    ld      d, 0 + (Bg_Bottom.size / 256)
     ld      b, 0 ; 256 bytes
-.loop_10:    
+.loop_20:    
     otir
     dec     d
-    jp      nz, .loop_10
+    jp      nz, .loop_20
+
 
     ret
 
@@ -108,18 +127,17 @@ Palette:
 ; MegaROM pages at 0x8000
 ; ------- Page 1
 	org	0x8000, 0xBFFF
-ImageData:
+Bg_Top:
     INCBIN "Images/mk-bg-top.sc5"
-.size:      equ $ - ImageData
+.size:      equ $ - Bg_Top
 	ds PageSize - ($ - 0x8000), 255
 
-; ; ------- Page 2
-; 	org	0x8000, 0xBFFF
-; ImageData_2:
-;     INCBIN "Images/aerofighters-xab"
-; .size:      equ $ - ImageData_2
-; .palette:   equ $ - 32
-; 	ds PageSize - ($ - 0x8000), 255
+; ------- Page 2
+	org	0x8000, 0xBFFF
+Bg_Bottom:
+    INCBIN "Images/mk-bg-bottom.sc5"
+.size:      equ $ - Bg_Bottom
+	ds PageSize - ($ - 0x8000), 255
 
 
 
